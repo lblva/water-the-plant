d3764e6dd42bd64486a4c471c0b7d7e864b2cf70
lblva <lola.batchalova@hotmail.com>
2024-12-18 16:02:02 +0100

    working on profile page

--- app/(tabs)/(index)/profile.tsx
+++ app/(tabs)/(index)/profile.tsx
@@ -1,4 +1,5 @@
-import { Image, StyleSheet, Text, Platform } from 'react-native';
+import { Image, StyleSheet, Text, View, TextInput } from 'react-native';
+import { Link } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import useUsers from '@/data/users';
 
@@ -14,7 +15,40 @@ export default function HomeScreen() {
     
   return (
     <SafeAreaView style={[styles.mainContainer]}>
-      <Text style={styles.title}> Profile </Text>
+      <Link href={`/`} style={[styles.arrow]}>
+        <Image source={require('@/assets/images/arrow.png')}  />
+      </Link>
+      <Text style={styles.title}> Account </Text>
+      <View style={[styles.profileImageContainer]}>
+        <Image source={require('@/assets/images/profile.png')} style={[styles.profileImage]} />
+      </View>
+      
+      <Text style={styles.inputTitle} >Name</Text>
+      <View style={styles.inputContainer}>
+      <Image
+        source={require('@/assets/images/naam-icon.png')} // Replace with your image path
+        style={styles.icon}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="name"
+        placeholderTextColor="gray"
+      />
+      </View>
+
+      <Text style={styles.inputTitle}>E-mail</Text>
+      <View style={styles.inputContainer}>
+      <Image
+        source={require('@/assets/images/email-icon.png')} // Replace with your image path
+        style={styles.icon}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="email"
+        placeholderTextColor="gray"
+      />
+    </View>
+      
     </SafeAreaView>
   );
 }
@@ -29,5 +63,44 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: 'bold',
         marginBottom: 20,
-    }
+    },
+    arrow: {
+        width: 34,
+        height: 24,
+        marginBottom: 10,
+    },
+    profileImageContainer: {
+        alignItems: 'center',
+        marginTop: 20,
+        marginBottom: 10,
+    },
+    profileImage: {
+        width: 120,
+        height: 120,
+    },
+    inputTitle: {
+        fontSize: 16,
+        marginTop: 20,
+    },
+    inputContainer: {
+      flexDirection: 'row', // Align image and input horizontally
+      alignItems: 'center', // Align items vertically within the container
+      borderWidth: 1,
+      height: 40,
+      borderColor: '#E8E8E8',
+      borderRadius: 5,
+      paddingHorizontal: 10,
+      marginVertical: 7,
+      backgroundColor: '#fff', // Optional: Add a background color
+    },
+    icon: {
+      width: 20, // Adjust width of the image
+      height: 20, // Adjust height of the image
+      marginRight: 10, // Space between image and TextInput
+    },
+    input: {
+      flex: 1, // Allow the TextInput to take the remaining space
+      fontSize: 16,
+      color: 'black',
+    },
 });
